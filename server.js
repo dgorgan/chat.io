d7a55368a066f8f8775495b2c8114de5a1bf5cbc
Dumitru Gorgan <dgorgan3@gmail.com>
2016-02-05 01:03:45 -0500

    reconfigured server.js

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+var http = require('http');
 var express = require('express')
 app = express();
 
+var jade = require('jade');
+var io = require('socket.io');
+var server = http.createServer(app);
+
+io = io.listen(server);
+server.listen(3000);
+
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.set("view options", { layout: false });
-app.configure(function() {
-    app.use(express.static(__dirname + '/public'));
+app.set("view options", {
+  layout: false
 });
 
-app.get('/', function(req, res){
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
   res.render('home.jade');
 });
-app.listen(3000);
