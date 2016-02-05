2814b2cf346bba432d20f117233cd5d1d5d25619
Dumitru Gorgan <dgorgan3@gmail.com>
2016-02-05 01:07:42 -0500

    integrated socket.io functionality on the server side

--- server.js
+++ server.js
@@ -21,3 +21,20 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.render('home.jade');
 });
+
+io.sockets.on('connection', function(socket) {
+  socket.on('setPseudo', function(data) {
+    socket.set('pseudo', data);
+  });
+
+  socket.on('message', function(message) {
+    socket.get('pseudo', function(error, name) {
+      var data = {
+        'message': message,
+        pseudo: name
+      };
+      socket.broadcast.emit('message', data);
+      console.log("user " + name + " send this : " + message);
+    })
+  });
+});
